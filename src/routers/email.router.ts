8ab2c5b5bbed58e14fe0125d12637ddb1d27761f
QunDev <qun942004@gmail.com>
2025-02-05 02:11:14 +0700

    update

--- src/routers/email.router.ts
+++ src/routers/email.router.ts
@@ -6,11 +6,11 @@ const router = express.Router()
 const emailController = new EmailController()
 
 router.get('/', asyncHandler(emailController.getEmails))
+router.get('/random', asyncHandler(emailController.getRandomEmail))
 router.get('/:id', asyncHandler(emailController.getEmail))
 router.post('/', asyncHandler(emailController.createEmails))
 router.put('/:id', asyncHandler(emailController.updateEmail))
 router.delete('/:id', asyncHandler(emailController.deleteEmail))
 router.delete('/', asyncHandler(emailController.deleteAllEmails))
-router.get('/random', asyncHandler(emailController.getRandomEmail))
 
 export default router
