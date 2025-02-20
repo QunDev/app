aaeed4d896f9f2ab2667e234fd9a9bd4fa5cb564
QunDev <qun942004@gmail.com>
2025-02-20 07:05:15 +0700

    update

--- src/validations/app.validation.ts
+++ src/validations/app.validation.ts
@@ -2,10 +2,10 @@ import { z } from 'zod'
 
 export const createAppSchema = z.object({
   name: z.string().min(1, 'App name is required'),
-  updated: z.boolean().optional(),
+  updated: z.string().optional(),
 })
 
 export const updateAppSchema = z.object({
   name: z.string().min(1, 'App name is required').optional(),
-  updated: z.boolean().optional(),
+  updated: z.string().optional(),
 })
